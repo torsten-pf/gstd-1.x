242a2be11e2707eb1acd0635a0357ebb9a5c285a
Miguel Taylor <miguel.taylor@ridgerun.com>
2021-05-11 07:57:49 +0800

    Fix unref order

--- libgstd/gstd_parser.c
+++ libgstd/gstd_parser.c
@@ -1035,8 +1035,8 @@ gstd_parser_pipeline_create_ref (GstdSession * session, gchar * action,
 
 create_error:
   GST_OBJECT_UNLOCK (session);
-pipeline_list_node_error:
   gst_object_unref (pipeline_list_node);
+pipeline_list_node_error:
   g_strfreev (tokens);
   return ret;
 }
@@ -1082,8 +1082,8 @@ gstd_parser_pipeline_delete_ref (GstdSession * session, gchar * action,
 
 pipeline_node_error:
   GST_OBJECT_UNLOCK (session);
-pipeline_list_node_error:
   gst_object_unref (pipeline_list_node);
+pipeline_list_node_error:
   return ret;
 }
 
@@ -1134,10 +1134,10 @@ gstd_parser_pipeline_play_ref (GstdSession * session, gchar * action,
 
 play_error:
   GST_OBJECT_UNLOCK (pipeline_node);
-state_node_error:
   gst_object_unref (state_node);
-pipeline_node_error:
+state_node_error:
   gst_object_unref (pipeline_node);
+pipeline_node_error:
   return ret;
 }
 
@@ -1188,9 +1188,9 @@ gstd_parser_pipeline_stop_ref (GstdSession * session, gchar * action,
 
 stop_error:
   GST_OBJECT_UNLOCK (pipeline_node);
-state_node_error:
   gst_object_unref (state_node);
-pipeline_node_error:
+state_node_error:
   gst_object_unref (pipeline_node);
+pipeline_node_error:
   return ret;
 }
